447d2d2e7f0e54dc6ead47407f9a9c22aeefd633
JacobLoredo <yeicob_loredo@hotmail.com>
2021-07-21 09:56:27 -0500

    prueba de inicio con taild

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container ">
+       <div class="grid grid-cols-4 bg-red-800">
+           <div>a</div>
+           <div>vb</div>
+           <div>bf</div>
+           <div>ds</div>
+       </div>
+    </div>
+</body>
+</html>
